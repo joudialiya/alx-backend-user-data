d9737e35bed8b25f1282af78485a43fdf727b131
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-06 21:29:12 +0000

    1.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,7 +26,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """class constructor"""
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
