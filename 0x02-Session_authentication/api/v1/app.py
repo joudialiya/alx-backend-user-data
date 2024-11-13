d6399cd79f6f28dfb22b7075a0bfa9f1efb14655
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-13 19:28:40 +0000

    0.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -41,7 +41,7 @@ def authentication():
             abort(401)
         if not auth.current_user(request):
             abort(403)
-        request.current_user = auth.current_user(request)
+    setattr(request, "current_user", auth.current_user(request))
 
 
 @app.errorhandler(401)
