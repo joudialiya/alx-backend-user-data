896c3a1d30ff46aded8009e536e8d72a9911d3ec
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-13 21:08:38 +0000

    9.

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -10,12 +10,11 @@ class SessionExpAuth(SessionAuth):
     """Session experation auth"""
     def __init__(self):
         """claass constructor"""
-        potential = os.getenv("SESSION_DURATION", '0')
+        potential = os.getenv("SESSION_DURATION")
         try:
             self.session_duration = int(potential)
         except Exception:
             self.session_duration = 0
-        super().__init__()
 
     def create_session(self, user_id=None):
         """create session"""
@@ -33,12 +32,14 @@ class SessionExpAuth(SessionAuth):
         if not session_id or type(session_id) is not str:
             return None
         session_object = SessionAuth.user_id_by_session_id.get(session_id)
+        if not session_object:
+            return None
         if self.session_duration <= 0:
             return session_object.get("user_id")
         created_at: datetime = session_object.get("created_at")
         if not created_at:
             return None
-        if (created_at + 
+        if (created_at +
                 timedelta(seconds=self.session_duration)
                 < datetime.now()):
             return None
