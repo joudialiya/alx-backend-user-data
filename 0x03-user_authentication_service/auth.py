c6a2e461bd93a4aba33d9972a4e448493d3d2c2d
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-21 21:06:35 +0000

    10.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,12 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
+
+
+def _generate_uuid() -> str:
+    """uuid wraper"""
+    return str(uuid4())
 
 
 def _hash_password(password: str) -> bytes:
@@ -28,3 +34,21 @@ class Auth:
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
             self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password, user.hashed_password)
+        except Exception:
+            return False
+
+    def create_session(self, email: str):
+        """Create session"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
