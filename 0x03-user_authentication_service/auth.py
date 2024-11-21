ef4343d724861b10b1850c04ca45bd05ceb21f47
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-21 21:10:38 +0000

    8. encode

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,11 +39,13 @@ class Auth:
         """Validate a user"""
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password, user.hashed_password)
+            return bcrypt.checkpw(
+                password.encode("utf-8"),
+                user.hashed_password)
         except Exception:
             return False
 
-    def create_session(self, email: str):
+    def create_session(self, email: str) -> str:
         """Create session"""
         try:
             user = self._db.find_user_by(email=email)
