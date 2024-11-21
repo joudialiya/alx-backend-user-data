47996be830305e77e2ed622d007f27a9b278ccd7
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-21 10:51:20 +0000

    2.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,4 +39,14 @@ class DB:
         user.hashed_password = hashed_password
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwarg) -> User:
+        """Find user"""
+        for attr in kwarg.keys():
+            if not hasattr(User, attr):
+                raise InvalidRequestError()
+        user = self._session.query(User).filter_by(**kwarg).first()
+        if not user:
+            raise NoResultFound
         return user
