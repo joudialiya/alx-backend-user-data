32797fbfe9a3c5de7ba20c03eb8e9bbe329862d7
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-21 10:17:19 +0000

    0. varchar(250)

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,7 +18,7 @@ class User(Base):
     # session_id, a nullable string
     # reset_token, a nullable string
     id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String)
-    reset_token = Column(String)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
