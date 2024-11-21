83c40ae265c4bd71f12e315a79307a199abc9853
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-21 20:26:23 +0000

    6-7.

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Flask app module
+"""
+from flask import jsonify, Flask, request
+from auth import Auth
+
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Say hi"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """Register a user"""
+    email = request.form["email"]
+    password = request.form["password"]
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
